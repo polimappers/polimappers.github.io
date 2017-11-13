bb1f98d70c01dfbb3508099659ada060ef9f57b1
Francesco Frassinelli <fraph24@gmail.com>
2017-11-13 20:02:28 +0100

    Loading/no events messages

--- rss.js
+++ rss.js
@@ -1,6 +1,7 @@
 let feeds = document.getElementsByClassName('rss');
 for (let i=0; i<feeds.length; i++) {
     let feed = feeds[i];
+    feed.innerHTML = '<i>Loading RSS feed...</i>'
     fetch(feed.dataset['source'])
         .then(response => response.text())
         .then(str => (new window.DOMParser()).parseFromString(str, 'text/xml'))
@@ -11,6 +12,11 @@ function parseRSS(feed, xml) {
     items = xml.querySelectorAll('rss > channel > item');
     let maxItems = feed.dataset['limit'];
     if (items.length < maxItems) maxItems = items.length;
+    if (maxItems == 0) {
+        feed.innerHTML = '<i>No events scheduled</i>';
+    } else {
+        feed.innerHTML = '';
+    }
     for (let i=0; i<maxItems; i++) {
         let item = items[i];
         let title = item.querySelector('title').textContent;
