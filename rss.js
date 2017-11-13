c533e646e7feea141e26ed2c2e11f1584a6de2f8
Francesco Frassinelli <fraph24@gmail.com>
2017-11-13 18:48:25 +0100

    Add EventBrite RSS

---
+++ rss.js
@@ -0,0 +1,28 @@
+let feeds = document.getElementsByClassName('rss');
+for (let i=0; i<feeds.length; i++) {
+    let feed = feeds[i];
+    fetch(feed.dataset['source'])
+        .then(response => response.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, 'text/xml'))
+        .then(xml => parseRSS(feed, xml));
+}
+
+function parseRSS(feed, xml) {
+    items = xml.querySelectorAll('rss > channel > item');
+    let maxItems = feed.dataset['limit'];
+    if (items.length < maxItems) maxItems = items.length;
+    for (let i=0; i<maxItems; i++) {
+        let item = items[i];
+        let title = item.querySelector('title').textContent;
+        let link = item.querySelector('link').textContent;
+        let description = item.querySelector('description').textContent;
+        let short = description;
+        if (feed.dataset.hasOwnProperty('delimiter')) {
+            short = short.substr(0, short.indexOf(feed.dataset['delimiter']));
+        }
+        let entry = `<h3>${title}</h3>
+        ${short}
+        <a href="${link}">Read more...</a>`;
+        feed.innerHTML += entry;
+    }
+}
